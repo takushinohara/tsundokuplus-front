8a4ec7107c60d3e45a1e286153dddf1eb33f5462
Taku Shinohara <shinohara.mc@gmail.com>
2022-12-21 17:53:20 +0900

    fix: make minor changes to UI design

--- pages/home.vue
+++ pages/home.vue
@@ -5,9 +5,8 @@
       <div class="flex justify-between items-end gap-4 mb-6">
         <div class="w-full md:max-w-md flex gap-2">
           <input
-            type="search"
             placeholder="Filter your books"
-            class="w-full flex-1 bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
+            class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
             v-model="state.filterKeyword"
             :disabled="!books.length"
           />
